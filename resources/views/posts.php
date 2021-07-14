c1f875d87b35a35d8d1f72fe4c56cb8b8e7aa39f
henrylemmon <henrylemmon@yahoo.com>
2021-07-13 20:18:27 -0700

    routing constraints

---
+++ resources/views/posts.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <title>Me Blog</title>
+</head>
+<body>
+<article>
+    <h1><a href="posts/my-first-post">My First Post</a></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dolorem eaque maiores veritatis voluptas? Alias ducimus illo iusto laboriosam, laudantium maxime minima nemo optio perferendis, recusandae, sapiente sit voluptatibus voluptatum!
+    </p>
+</article>
+
+<article>
+    <h1><a href="posts/my-second-post">My Second Post</a></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dolorem eaque maiores veritatis voluptas? Alias ducimus illo iusto laboriosam, laudantium maxime minima nemo optio perferendis, recusandae, sapiente sit voluptatibus voluptatum!
+    </p>
+</article>
+
+<article>
+    <h1><a href="posts/my-third-post">My Third Post</a></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dolorem eaque maiores veritatis voluptas? Alias ducimus illo iusto laboriosam, laudantium maxime minima nemo optio perferendis, recusandae, sapiente sit voluptatibus voluptatum!
+    </p>
+</article>
+</body>
+</html>

78fcc475f3c19ff60e8aa9ff95dc538a77528a44
henrylemmon <henrylemmon@yahoo.com>
2021-07-14 11:17:54 -0700

    caching episode 10 complete

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,13 @@ Route::get('/', function () {
 });
 
 Route::get('posts/{post}', function ($slug) {
-    $path = __DIR__ . "/../resources/posts/{$slug}.html";
-
-    if (!file_exists($path)) {
+    if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}.html")) {
         return redirect('/');
     }
 
-    $post = file_get_contents($path);
+    $post = cache()->remember("posts.{$slug}", 5, function() use ($path) {
+        return file_get_contents($path);
+    });
 
     return view('post', [
         'post' => $post,

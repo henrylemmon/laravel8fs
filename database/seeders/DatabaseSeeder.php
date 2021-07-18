121ffe4a7783c4fa87dbf16c6f8467f9b3eeafde
henrylemmon <henrylemmon@yahoo.com>
2021-07-18 10:55:22 -0700

    30 complete and got html and css

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create([
+            'name' => 'Henry',
+            'username' => 'henrylemmon',
+            'email' => 'henry@mail.com',
+        ]);
+
+        $poodle = Category::factory()->create([
+            'name' => 'Poodle',
+            'slug' => 'poodle',
+        ]);
+
+        $person = Category::factory()->create([
+            'name' => 'Person',
+            'slug' => 'person',
+        ]);
+
+        Post::factory(2)->create([
+            'user_id' => $user->id,
+            'category_id' => $poodle->id,
+        ]);
+
+        Post::factory(2)->create([
+            'user_id' => $user->id,
+            'category_id' => $person->id,
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,16 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use App\Models\Category;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    // traded out for clockwork
-    /*DB::listen(function ($query) {
-        logger($query->sql, $query->bindings);
-    });*/
-
     return view('posts', [
-        'posts' => Post::with('category')->get(),
+        'posts' => Post::latest()/*->with('category', 'author')*/->get(),
     ]);
 });
 
@@ -37,6 +22,12 @@ Route::get('posts/{post:slug}', function (Post $post) {
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts,
+        'posts' => $category->posts/*->load(['category', 'author'])*/,
+    ]);
+});
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts/*->load(['category', 'author'])*/,
     ]);
 });

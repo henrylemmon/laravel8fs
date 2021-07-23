9b2d5c21fea9c4b23d17fb962f5dc1b315f910b9
henrylemmon <henrylemmon@yahoo.com>
2021-07-22 20:32:05 -0700

    43 complete

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,15 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
-use App\Models\Category;
-use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
     public function index()
     {
         return view('posts.index', [
-            'posts' => Post::latest()->filter(request(['search', 'category']))->get(),
+            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get(),
         ]);
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,7 @@
 <?php
 
-use App\Models\Post;
-use App\Models\User;
-use App\Models\Category;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', [PostsController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostsController::class, 'show']);
-
-Route::get('authors/{author:username}', function (User $author) {
-    return view('posts.index', [
-        'posts' => $author->posts/*->load(['category', 'author'])*/,
-    ]);
-});
